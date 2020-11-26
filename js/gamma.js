d3e548eca51df9ed14d28bb66cf33254c94592c1
Alexander Wyndace Vasilev <wyndace@hotmail.com>
2020-11-27 00:28:36 +0500

    Updating the Gamma decoder
    Added new features, but you can only choose through the code

--- js/gamma.js
+++ js/gamma.js
@@ -4,11 +4,11 @@ document.querySelector('#key').addEventListener('change', () => {
     pass = document.querySelector('#key').value.split('');
 });
 
-button.addEventListener('click', () => gamma());
+button.addEventListener('click', () => gammaEach()); //Меняем пока здесь
 
+//Просто разкодер
 const gamma = () => {
-  console.log(task);
-  console.log(pass);
+   answer.innerHTML = 'Твой ответ: ';
     task.forEach(letter => {
       letter = letter.charCodeAt();
       pass.forEach(passSymbol => {
@@ -18,3 +18,50 @@ const gamma = () => {
       });
     });
 }
+//Перебор всех значений аски и вывод их
+const gammaRounds = () => {
+  answer.innerHTML = ''
+      for (passSymbol = 32; passSymbol < 126; passSymbol++){
+      answer.innerHTML += '<br>Твой ответ: ';
+      task.forEach(letter => {
+      letter = letter.charCodeAt();
+      letter ^= passSymbol;
+      answer.innerHTML += String.fromCharCode(letter);
+    });
+    answer.innerHTML += ' ASCII Символ: ' + String.fromCharCode(passSymbol) + '<br>';
+      };
+}
+
+//Каждая с каждой
+const gammaEach = () => {
+  answer.innerHTML = 'Твой ответ: ';
+  console.log(task.length, pass.length);
+  if (task.length < pass.length) {
+    for (let i = 0; i < pass.length; i++){
+        let passSymbol = pass[i].charCodeAt();
+        task.forEach(letter => {
+          letter = letter.charCodeAt();
+          letter ^= passSymbol;
+          answer.innerHTML += String.fromCharCode(letter);
+      });
+    }
+  } else if (task.length > pass.length) {
+      for (let i = 0; i < task.length; i++){
+        let letter = task[i].charCodeAt();
+        pass.forEach(passSymbol => {
+          passSymbol = passSymbol.charCodeAt();
+          letter ^= passSymbol;
+          answer.innerHTML += String.fromCharCode(letter);
+      });
+    }
+    console.log('yes, big');
+  } else {
+    console.log('yes, equals')
+    for (let i = 0; i < task.length; i++){
+      let letter = task[i].charCodeAt();
+      let passSymbol = pass[i].charCodeAt();
+      letter ^= passSymbol;
+      answer.innerHTML += String.fromCharCode(letter);
+    }
+  }
+};
