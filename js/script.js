3428a053bc6febf311a29dcfb3ee732867e2819b
Alexander Wyndace Vasilev <wyndace@hotmail.com>
2020-11-26 22:55:45 +0500

    Creating a project
    hey hey boy! I have created a new project. :+1:

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use script";
+const button = document.querySelector('.button');
+const answer = document.querySelector(".answer");
+let task = document.querySelector('#task').value;
+let taskBinary = '';
+
+document.querySelector('#task').addEventListener('change', () => {
+    task = document.querySelector('#task').value
+});
+
+
+
+
