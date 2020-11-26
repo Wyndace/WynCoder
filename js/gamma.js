926272379a74fa38cf3ad34a68bc52b0cadd0730
Alexander Wyndace Vasilev <wyndace@hotmail.com>
2020-11-26 23:22:28 +0500

    Added Gamma Encoder
    Okey, I made it!

--- js/gamma.js
+++ js/gamma.js
@@ -1,5 +1,20 @@
+let pass = document.querySelector('#key').value.split('');
+
+document.querySelector('#key').addEventListener('change', () => {
+    pass = document.querySelector('#key').value.split('');
+});
+
+button.addEventListener('click', () => gamma());
+
 const gamma = () => {
-    task.forEach(task => {
-        
+  console.log(task);
+  console.log(pass);
+    task.forEach(letter => {
+      letter = letter.charCodeAt();
+      pass.forEach(passSymbol => {
+      passSymbol = passSymbol.charCodeAt();
+      letter ^= passSymbol;
+      answer.innerHTML += String.fromCharCode(letter);
+      });
     });
 }
--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 "use script";
 const button = document.querySelector('.button');
 const answer = document.querySelector(".answer");
-let task = document.querySelector('#task').value;
-let taskBinary = '';
+let task = document.querySelector('#task').value.split('');
 
 document.querySelector('#task').addEventListener('change', () => {
-    task = document.querySelector('#task').value
+    task = document.querySelector('#task').value.split('');
 });
 
 
